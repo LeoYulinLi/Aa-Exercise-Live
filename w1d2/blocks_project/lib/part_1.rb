ae4aced7b16d94e5433f0d57f9e56d654ca2d974
LeoYulinLi <LeoYulinLi@gmail.com>
2020-04-07 17:40:03 -0700

    Some reformatting

--- w1d2/blocks_project/lib/part_1.rb
+++ w1d2/blocks_project/lib/part_1.rb
@@ -1,9 +1,9 @@
 def select_even_nums(array)
-  array.select { |e| e.even? }
+  array.select(&:even?)
 end
 
 def reject_puppies(arr)
-    arr.reject {|ele| ele['age'] <= 2}
+  arr.reject { |ele| ele['age'] <= 2 }
 end
 
 def count_positive_subarrays(arrays)
@@ -11,12 +11,12 @@ def count_positive_subarrays(arrays)
 end
 
 def aba_translate(str)
-    vowels = "aeiou"
-    str.map do |char|
-        if vowels.include?(char)
-            "#{char}b#{char}"
-        else
-          char 
-        end
-      end
+  vowels = "aeiou"
+  str.map do |char|
+    if vowels.include?(char)
+      "#{char}b#{char}"
+    else
+      char
+    end
+  end
 end
