9851a23053ae76d08d21e9b32cc3448843fe63d7
LeoYulinLi <LeoYulinLi@gmail.com>
2020-04-10 16:31:59 -0700

    Oops wrong file location

--- w1d5/rspec_exercise_5/lib/exercise.rb
+++ w1d5/rspec_exercise_5/lib/exercise.rb
@@ -74,36 +74,3 @@ def silly_syllables(sent)
     end
   end.join(" ") # this is possible because do...end is a block
 end
-
-def silly_talk(sentence)
-  sentence.split.map do |word|
-    if "aeiou".include?(word[-1])
-      word + word[-1]
-    else
-      word.chars.map { |char| "aeiou".include?(char) ? char + "b" + char : char }.join
-    end
-  end.join(" ")
-end
-
-# p silly_talk('Kids like cats and dogs') # "Kibids likee cabats aband dobogs"
-# p silly_talk('Stop that scooter') # "Stobop thabat scobooboteber"
-# p silly_talk('They can code') # "Thebey caban codee"
-# p silly_talk('He flew to Italy') # "Hee flebew too Ibitabaly"
-
-def compress(string)
-  counter = 1
-  result = ""
-  string.each_char.with_index do |char, i|
-    if char != string[i + 1]
-      result += "#{char}#{counter if counter > 1}"
-      counter = 1
-    else
-      counter += 1
-    end
-  end
-  result
-end
-
-p compress('aabbbbc')   # "a2b4c"
-p compress('boot')      # "bo2t"
-p compress('xxxyxxzzzz')# "x3yx2z4"
