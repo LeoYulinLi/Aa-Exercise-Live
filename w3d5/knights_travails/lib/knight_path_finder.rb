870436e8dcf4a859c2286923bde2cda0190eb3e7
LeoYulinLi <LeoYulinLi@gmail.com>
2020-04-24 16:13:30 -0700

    W3D5 done

---
+++ w3d5/knights_travails/lib/knight_path_finder.rb
@@ -0,0 +1,42 @@
+require_relative "../../poly_tree_node/lib/00_tree_node.rb"
+
+class KnightPathFinder
+  def initialize(start)
+    @start = start
+    @considered_positions = [start]
+  end
+
+  def self.valid_moves(pos)
+    x, y = pos
+    potential_positions = (-1..1).flat_map do |i|
+      (-1..1).map do |j|
+        [i + x, j + y]
+      end
+    end
+    potential_positions.select { |(x, y)| (0...8).include?(x) && (0...8).include?(y) }
+  end
+
+  def new_move_positions(pos)
+    KnightPathFinder.valid_moves(pos).reject { |p| @considered_positions.include?(p) }
+  end
+
+  def build_move_tree
+    tree = PolyTreeNode.new(@start)
+    queue = [tree]
+    until queue.empty?
+      current_node = queue.shift
+      queue += new_move_positions(current_node.value).map do |p|
+        new_node = PolyTreeNode.new(p)
+        @considered_positions << new_node.value
+        new_node.parent = current_node
+        new_node
+      end
+    end
+    tree
+  end
+end
+
+k = KnightPathFinder.new([0, 0])
+t =  k.build_move_tree
+
+p t
--- w3d5/poly_tree_node/lib/00_tree_node.rb
+++ w3d5/poly_tree_node/lib/00_tree_node.rb
@@ -1,3 +1,61 @@
 class PolyTreeNode
 
+  attr_reader :parent, :children, :value
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(node)
+    @parent.children.delete_if { |n| n == self } unless parent.nil?
+    @parent = node
+    node.children << self unless node.nil?
+  end
+
+  def add_child(node)
+    node.parent = self
+  end
+
+  def remove_child(node)
+    if node.parent == self
+      node.parent = nil
+    else
+      raise "Error: This node doesn't belong to the parent"
+    end
+  end
+
+  def dfs(target)
+    return self if value == target
+    return nil if children.empty?
+
+    children.each do |node|
+      result = node.dfs(target)
+      return result unless result.nil?
+    end
+    nil
+  end
+
+  def bfs(target)
+
+    nodes = [self]
+
+    until nodes.empty?
+      checked = nodes.shift
+      return checked if checked.value == target
+
+      checked.children.each { |node| nodes << node }
+    end
+    nil
+  end
+
+  def inspect
+    "#{value}"
+  end
+
+  def to_s
+    "#{value}"
+  end
+
 end
